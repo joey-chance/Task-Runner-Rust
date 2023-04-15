6706ca341456019e3ebd2fc518b772eceadcff78
joey-chance <joey.chance@u.nus.edu>
2023-04-15 23:56:44 +0800

    Added foundation for logic. Todo: fix data structures

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::{
     collections::{HashMap, VecDeque},
     time::Instant,
+    sync::atomic::{AtomicU64, Ordering},
+    sync::Arc
 };
 
 use task::{Task, TaskType};
+
+// use futures::future::join_all;
+use tokio::sync::{mpsc, Barrier, Mutex, Semaphore};
 
 fn main() {
     let (seed, starting_height, max_children) = get_args();
@@ -13,29 +18,38 @@ fn main() {
         seed, starting_height, max_children
     );
 
+    // protect both with a mutex
     let mut count_map = HashMap::new();
+    // let mut count_map = Arc::new(Mutex::new(HashMap::new()));
     let mut taskq = VecDeque::from(Task::generate_initial(seed, starting_height, max_children));
 
-    let mut output: u64 = 0;
+    // output can be safely accessed from multiple threads
+    let output = AtomicU64::new(0);
 
     let start = Instant::now();
     while let Some(next) = taskq.pop_front() {
+        // process initial set of tasks
+        // wrap it in a tokio task
+        // new tasks won't be added to the taskq but instead the handle of the task will be saved in another vector
+        // let count_map = count_map.clone();
         *count_map.entry(next.typ).or_insert(0usize) += 1;
-        let result = next.execute();
-        output ^= result.0;
-        taskq.extend(result.1.into_iter());
+        let handle = tokio::spawn(async move {
+            let result = next.execute();
+            // output.fetch_xor(result.0, Ordering::SeqCst); // DONE
+            // taskq.extend(result.1.into_iter());
+        });
     }
     let end = Instant::now();
 
     eprintln!("Completed in {} s", (end - start).as_secs_f64());
 
-    println!(
-        "{},{},{},{}",
-        output,
-        count_map.get(&TaskType::Hash).unwrap_or(&0),
-        count_map.get(&TaskType::Derive).unwrap_or(&0),
-        count_map.get(&TaskType::Random).unwrap_or(&0)
-    );
+    // println!(
+    //     "{},{},{},{}",
+    //     output.load(Ordering::Relaxed),
+    //     count_map.get(&TaskType::Hash).unwrap_or(&0),
+    //     count_map.get(&TaskType::Derive).unwrap_or(&0),
+    //     count_map.get(&TaskType::Random).unwrap_or(&0)
+    // );
 }
 
 // There should be no need to modify anything below
