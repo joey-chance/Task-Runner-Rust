614979f047a6ff6a4ebce450dce35b270babff69
joey-chance <joey.chance@u.nus.edu>
2023-04-16 14:02:10 +0800

    Changed data structures for tokio::spawn. Todo: add child tasks somewhere

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::{
     collections::{HashMap, VecDeque},
     time::Instant,
     sync::atomic::{AtomicU64, Ordering},
-    sync::Arc
+    sync::Arc,
+    sync::Mutex
 };
 
 use task::{Task, TaskType};
 
 // use futures::future::join_all;
-use tokio::sync::{mpsc, Barrier, Mutex, Semaphore};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let (seed, starting_height, max_children) = get_args();
 
     eprintln!(
@@ -19,37 +20,40 @@ fn main() {
     );
 
     // protect both with a mutex
-    let mut count_map = HashMap::new();
-    // let mut count_map = Arc::new(Mutex::new(HashMap::new()));
+    // let mut count_map = HashMap::new();
+    let count_map:Arc<Mutex<HashMap<TaskType, usize>>> = Arc::new(Mutex::new(HashMap::new()));
     let mut taskq = VecDeque::from(Task::generate_initial(seed, starting_height, max_children));
 
     // output can be safely accessed from multiple threads
-    let output = AtomicU64::new(0);
+    let output = Arc::new(AtomicU64::new(0));
 
     let start = Instant::now();
     while let Some(next) = taskq.pop_front() {
         // process initial set of tasks
         // wrap it in a tokio task
         // new tasks won't be added to the taskq but instead the handle of the task will be saved in another vector
-        // let count_map = count_map.clone();
-        *count_map.entry(next.typ).or_insert(0usize) += 1;
+        let count_map = count_map.clone();
+        let output = output.clone();
+        let mut pending_tasks = Vec::new();
         let handle = tokio::spawn(async move {
-            let result = next.execute();
-            // output.fetch_xor(result.0, Ordering::SeqCst); // DONE
+            *count_map.lock().unwrap().entry(next.typ).or_insert(0usize) += 1;
+            let result = async {next.execute()}.await;
+            output.fetch_xor(result.0, Ordering::SeqCst); // DONE
             // taskq.extend(result.1.into_iter());
         });
+        pending_tasks.push(handle);
     }
     let end = Instant::now();
 
     eprintln!("Completed in {} s", (end - start).as_secs_f64());
 
-    // println!(
-    //     "{},{},{},{}",
-    //     output.load(Ordering::Relaxed),
-    //     count_map.get(&TaskType::Hash).unwrap_or(&0),
-    //     count_map.get(&TaskType::Derive).unwrap_or(&0),
-    //     count_map.get(&TaskType::Random).unwrap_or(&0)
-    // );
+    println!(
+        "{},{},{},{}",
+        output.load(Ordering::Relaxed),
+        count_map.lock().unwrap().get(&TaskType::Hash).unwrap_or(&0),
+        count_map.lock().unwrap().get(&TaskType::Derive).unwrap_or(&0),
+        count_map.lock().unwrap().get(&TaskType::Random).unwrap_or(&0)
+    );
 }
 
 // There should be no need to modify anything below
